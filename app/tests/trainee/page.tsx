a0cb3ff08ae3969f1caeac4a7d28a322e0e4d5f3
Олександр Маркович <markoleks08@gmail.com>
2025-06-23 14:17:38 +0300

    + photos of serbing tablewear
    I also forgot to type "use client" on top of the main test file

--- app/tests/trainee/page.tsx
+++ app/tests/trainee/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState, useEffect, useRef } from 'react'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
