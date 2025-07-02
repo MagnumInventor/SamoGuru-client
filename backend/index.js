2b54c803661348196619444c1a6f9b39015035a7
Олександр Маркович <markoleks08@gmail.com>
2025-07-02 14:30:46 +0300

    back to FULL auth

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./db');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+// Підключення до бази
+connectDB();
+
+// Роут (тимчасово)
+app.get('/', (req, res) => {
+  res.send('Samoguru API working');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
