73d172e6b412e259080d12adbf9a8dd84d72d129
Oleksandr Markovych <markoleks08@gmail.com>
2025-07-03 17:09:51 +0300

    Proper user register data User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,6 +4,14 @@ const UserSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
   phone: { type: String, unique: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/^\S+@\S+\.\S+$/, 'Невірний формат електронної пошти'],
+  },
   password: String,
   role: { type: String, enum: ['waiter', 'helper', 'admin'], default: 'waiter' },
 });
