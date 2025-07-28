3df48ba2e36794555d001f2e33fdf25e0593c15a
Олександр Маркович <markoleks08@gmail.com>
2025-07-28 22:16:47 +0300

    BACK to old new

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,18 @@
-//backend/routes/auth.route.js
 import express from "express";
-import { login, logout, signup, verifyEmail, forgotPassword } from "../controllers/auth.controller.js";
-
+import {
+	login,
+	logout,
+	signup,
+	verifyEmail,
+	forgotPassword,
+	resetPassword,
+	checkAuth,
+} from "../controllers/auth.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/check-auth", verifyToken, checkAuth);
 
 router.post("/signup", signup);
 router.post("/login", login);
@@ -11,5 +20,7 @@ router.post("/logout", logout);
 
 router.post("/verify-email", verifyEmail);
 router.post("/forgot-password", forgotPassword);
+
+router.post("/reset-password/:token", resetPassword);
 
 export default router;
