d87eba987fe189b32133a431400762c50d9a73d9
Oleksandr Markovych <markoleks08@gmail.com>
2025-08-25 00:55:55 +0300

    Use client page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/app/components/ui/card";
 import { Button } from "@/app/components/ui/button";
