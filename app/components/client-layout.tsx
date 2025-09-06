e1d30c58296d434399b45e2425a369a22a47fa19
MagnumInventor <markoleks08@gmail.com>
2025-09-06 12:38:24 +0300

    Перевірка верифікації (email)

--- app/components/client-layout.tsx
+++ app/components/client-layout.tsx
@@ -5,11 +5,11 @@ import { Navigation } from "@/app/components/navigation"
 import { Footer } from "@/app/components/footer"
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
-  const { user } = useAuthStore()
+  const { user, isVerified } = useAuthStore()
 
   return (
     <div className="min-h-screen flex flex-col">
-      {user && <Navigation />}
+      {user && isVerified && <Navigation />}
       <main className="flex-1 bg-gray-50">{children}</main>
       <Footer />
     </div>
